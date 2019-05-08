1877319b30a9ad6f498259ac96bb5d26c202b3c9
danilor@bompracredito.com.br <danilor@bompracredito.com.br>
2019-05-08 15:18:00 -0300

    Inclusão do projeto de testes

---
+++ DesafioDojo.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DesafioDojo.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DesafioDojo.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5c552ba6-0b7e-449c-bcfa-5acd9cb2b0b5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DesafioDojo.Test/SeguroTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesafioDojo.Test
+{
+    [TestClass]
+    public class SeguroTest
+    {
+        [TestMethod]
+        public void DeveRetornarValorCorretoDeCalculo()
+        {
+            //Arrange
+            Seguro seguro = new Seguro()
+            {
+                Valor = 34000,
+                EstadoCivil = 0,
+                Garagem = false,
+                Historico = 1,
+                Idade = 27,
+                ItensSeguranca = false,
+                Sexo = 0,
+                CEP = "06065020"
+            };
+            //Act
+            double valorFranquiaCalculada = seguro.Calcular().Result;
+            //Assert
+            Assert.AreEqual(2971.085, valorFranquiaCalculada);
+        }
+    }
+}
